1ffac498447f128d173a1013f416dfee478d4f7d
tommy1013 <tomizi1013@yahoo.co.jp>
2017-09-05 15:36:26 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/toyamayuusuke/.ssh/keypair.pem']
+                  keys: ['/Users/tomi/.ssh/keypair.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
