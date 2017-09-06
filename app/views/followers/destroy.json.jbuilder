3abe3f2673f07b49849f27c04de2fd13ebc18731
tommy1013 <tomizi1013@yahoo.co.jp>
2017-09-06 13:43:08 +0900

    add view

---
+++ app/views/followers/destroy.json.jbuilder
@@ -0,0 +1,3 @@
+json.user_id            current_user.id
+json.followed_user_id   @user.id
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  root                          "items#index"
   devise_for :users
-  root 'items#index'
+
+  resources :my_pages,           only: :index do
+    collection do
+      get 'sell_list'
+      get 'buy_list'
+    end
+  end
+
+  resources  :users,             only: :show do
+    resources :buyers,           only: :index
+  end
+
+  resources  :items,             only: [:index, :show, :new, :create] do
+    collection do
+      get 'search'
+    end
+    resources :buyers,           only: [:new, :create]
+  end
+
+  resources :sub_categories,     only: :index
+  resources :categories,         only: :index
+  resources :followers,          only: [:index, :create, :destroy]
+  resources :likes,              only: [:index, :create, :destroy]
+
 end
