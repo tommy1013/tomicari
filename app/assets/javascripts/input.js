9bfa92fe99d4a8084ef299ed66a48af80e91f200
tommy1013 <tomizi1013@yahoo.co.jp>
2017-09-26 15:18:55 +0900

    fix carrierwave

---
+++ app/assets/javascripts/input.js
@@ -0,0 +1,12 @@
+$(function(){
+
+
+  var myFunc = function() {
+              $("#item_images").click();
+          };
+
+          $(".sell_form__drop_box").on("click", function() {
+              myFunc();
+          });
+
+});
