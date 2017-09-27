3206caf239fad0805b6b56ff653f5d1bd06a842e
tommy1013 <tomizi1013@yahoo.co.jp>
2017-09-27 14:15:29 +0900

    rview

--- db/migrate/20170911080023_add_item_ref_to_item_image.rb
+++
@@ -1,6 +0,0 @@
-class AddItemRefToItemImage < ActiveRecord::Migration[5.0]
-  def change
-    add_column :item_images, :prototype, :string
-    add_column :item_images, :references, :string
-  end
-end
